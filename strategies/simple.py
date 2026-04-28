6166353dbddf153f75c1caaa8dac65a29a00984e
Rezgen <joe@robertsfamily.name>
2026-04-28 17:52:21 +1200

    Update simple.py

--- strategies/simple.py
+++ strategies/simple.py
@@ -1,1 +1,8 @@
+from .base import BaseStrategy
 
+class SimpleStrategy(BaseStrategy):
+    def __init__(self, price=10):
+        self.price = price
+
+    def choose_price(self, state):
+        return self.price
